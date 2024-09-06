46b0cbaa7a3fd72aed077ba26337da9b0c905e4d
Jeremy Rebello <j.rebello1@gmail.com>
2024-09-06 07:17:48 +0700

    neural network first commit

---
+++ neural_network.py
@@ -0,0 +1,31 @@
+import numpy
+
+class neuralNetwork:
+    
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.innodes = inputnodes
+        self.hnodes = hiddennodes
+        self. onnodes = outputnodes
+        
+        # learning rate
+        self.lr = learningrate
+        pass
+    
+    def train():
+        pass
+    
+    def query():
+        pass
+    
+    
+# number of input, hidden and output nodes
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+
+# learning rate is 0.3
+learning_rate = 0.3
+
+# create instance of neural network
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
