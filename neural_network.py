90bd56b910d1f9ff36ee46bd3a03a1b0320e292c
Jeremy Rebello <11021289+the-traplord@users.noreply.github.com>
2024-09-21 08:42:02 +0700

    kjyf

--- neural_network.py
+++ neural_network.py
@@ -1,24 +1,55 @@
 import numpy
+import scipy.special # scipy.special for the sigmoid function expit()
 
 class neuralNetwork:
     
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        
         # set number of nodes in each input, hidden, output layer
-        self.innodes = inputnodes
-        self.hnodes = hiddennodes
-        self. onnodes = outputnodes
+        self.inodes     = inputnodes
+        self.hnodes     = hiddennodes
+        self.onnodes    = outputnodes
+        
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.normal(0.0, pow(self.inodes - 0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.hnodes - 0.5), (self.onodes, self.hnodes))
         
         # learning rate
         self.lr = learningrate
-        pass
+        
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
     
+    # Train the Neural Network
     def train():
         pass
     
-    def query():
-        pass
+    # Query the Neutal Network
+    def query(self, inputs_list):
+        
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        
+        # Calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        
+        # Calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
     
-    
+        # Calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        
+        # calculate signsls into the final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        
+        # Calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        
+        return final_outputs
+        
 # number of input, hidden and output nodes
 input_nodes = 3
 hidden_nodes = 3
